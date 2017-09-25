8bb31dd34ebe6f74bf38b5bc5d255042278cc216
svitaluc <svitaluc@gmail.com>
2017-09-25 11:45:50 -0500

    in-class work 9/25

---
+++ src/snake.js
@@ -0,0 +1,40 @@
+//snake.js
+
+/** @class Snake
+ * The snake in a Snake game
+ */
+export default class Snake {
+    constructor(x, y, segments) {
+        this.body = [];
+        for(var i = 0; i < segments; i++){
+            this.body.push({
+                x: x - i,
+                y:y
+            });
+        }
+        this.direction = 'right';
+    }
+    checkForConsumption(food){
+
+    }
+    update(){
+        //Did we smack a wall?
+        //Did we eat ourselves?
+        //Did we eat food?
+        //Do we need to grow?
+    }
+
+    /** @function render
+     * Render the snake
+     * @param ctx
+     */
+    render(ctx){
+        this.body.forEach(function(segment){
+            ctx.save();
+            ctx.fillStyle = 'green';
+            ctx.fillRect(
+                segment.x, segment.y, 1, 1);
+            ctx.restore();
+        })
+    }
+}
